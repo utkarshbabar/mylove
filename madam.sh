0ac4c78c447b6bda80e3b39551b4893faabc8370
utkarshbabar <utkarshbabarlaptop2006@gmail.com>
2025-07-27 03:30:57 -0400

    Add files via upload

---
+++ madam.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+echo "hello i am utkarsh"
+echo "enter the reply (hi /hello)>>"
+read enter 
+echo "hello baby "
+echo "enter the password --->>>>>>"
+read a
+
+
+if [[ "$a" == "21122006" ]]; then
+
+echo "i love you  madam ,love you s much madam"
+echo "   __   __"
+sleep 2
+echo "  /  \_/  \\"
+sleep 2
+echo "  \       //"
+sleep 2
+echo "   \     //"
+sleep 2
+echo "    \   //"
+sleep 2
+echo "     \ //"
+sleep 2
+echo "      V/"
+
+
+echo "i love you so much madam "
+echo "reply ----->>>"
+read b
+echo "i love you so much madam "
+
+
+else 
+echo "bye,but i love you so much madam"
+echo "I"
+echo "L"
+echo "O"
+echo "V"
+echo "E"
+echo ""
+sleep 2
+echo "Y"
+echo "O"
+echo "U"
+echo ""
+sleep 2
+echo "M"
+echo "A"
+echo "D"
+echo "A"
+echo "M"
+echo ""
+fi
